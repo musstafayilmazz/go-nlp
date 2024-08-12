cc08522d37b7642fad2d782cadd71c13a60b491a
mustafai <musstafayilmaz96@gmail.com>
2024-08-12 18:35:34 +0300

    verbchunk mini project

--- gopos.go
+++ gopos.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/jdkato/prose/tag"
 	"github.com/jdkato/prose/tokenize"
+	"strings"
 )
 
 func main() {
@@ -19,8 +20,11 @@ func main() {
 	}
 
 	fmt.Println(nounchunker(mytext))
+	fmt.Println(verbchunker(mytext))
+
 }
 
+// nounchunker add nouns in given string to a list and returns it
 func nounchunker(text string) []string {
 	var nounList []string
 	// Tokenize
@@ -36,4 +40,22 @@ func nounchunker(text string) []string {
 		}
 	}
 	return nounList
+}
+
+// verbchunker add nouns in given string to a list and returns it
+func verbchunker(text string) []string {
+	var verbList []string
+	// Tokenize
+	mytokens := tokenize.NewTreebankWordTokenizer().Tokenize(text)
+
+	//Tag
+	postagger := tag.NewPerceptronTagger()
+
+	// Append to list
+	for _, token := range postagger.Tag(mytokens) {
+		if strings.HasPrefix(token.Tag, "V") {
+			verbList = append(verbList, token.Text)
+		}
+	}
+	return verbList
 }
